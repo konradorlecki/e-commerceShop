6d78d7fc239ad08837149a66a282010590828339
Konrad <konradorlecki6@wp.pl>
2019-10-25 14:14:49 +0200

    Updates

---
+++ precache-manifest.fcc45c219f6904b66cbbb77818433fac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35b08470a299dd98d8d8be091ca38303",
+    "url": "/e-commerceShop/index.html"
+  },
+  {
+    "revision": "d8c0612182c98c743ccb",
+    "url": "/e-commerceShop/static/css/main.2a465e9a.chunk.css"
+  },
+  {
+    "revision": "31423cfd245869b1005e",
+    "url": "/e-commerceShop/static/js/2.d8783626.chunk.js"
+  },
+  {
+    "revision": "d8c0612182c98c743ccb",
+    "url": "/e-commerceShop/static/js/main.86ad77f0.chunk.js"
+  },
+  {
+    "revision": "e86f1fa06b06e7bde7fa",
+    "url": "/e-commerceShop/static/js/runtime-main.29303b1f.js"
+  }
+]);
